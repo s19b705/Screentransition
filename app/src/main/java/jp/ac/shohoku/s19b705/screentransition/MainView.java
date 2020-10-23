c8e08da12bfa993c5b8d45eb4f829b3af850be4e
s19B705 <s19b705@shohoku.ac.jp>
2020-10-23 13:00:56 +0900

    疲れた...僕..1人で...頑張って...えらい...!!

--- app/src/main/java/jp/ac/shohoku/s19b705/screentransition/MainView.java
+++ app/src/main/java/jp/ac/shohoku/s19b705/screentransition/MainView.java
@@ -10,12 +10,12 @@ import android.view.MotionEvent;
 import android.view.View;
 
 /**
- 13 * 一つの Activty，一つの View で状態遷移する場合
- 14 * Created by utsumi on 2015/10/20.
- 15 */
+* 一つの Activty，一つの View で状態遷移する場合
+*/
 public class MainView extends View{
     public final int FIRST = 1; //状態を表す定数 1
     public final int SECOND = 2; //状態を表す定数 2
+    public final int THIRD = 3; //状態を表す定数 2
 
     int state; //状態を表す変数
 
@@ -32,18 +32,38 @@ public class MainView extends View{
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Paint p = new Paint();
+        Paint p1 = new Paint();
+        Paint p2 = new Paint();
+        Paint p3 = new Paint();
 
-        if(state == FIRST) { //状態 1 の場合の描画
-            p.setColor(Color.BLUE);
+        if(state == FIRST) {
+            p1.setColor(Color.BLUE);
+            p2.setColor(Color.RED);
+            p3.setColor(Color.GREEN);p1.setColor(Color.BLUE);
             canvas.drawARGB(255, 255, 255, 255);
-            canvas.drawRect(100, 100, 300, 200, p);
-        } else if (state == SECOND){ //状態 2 の場合の描画
-            p.setColor(Color.RED);
+            canvas.drawRect(100, 100, 300, 200, p1);
+            canvas.drawRect(400, 100, 600, 200, p2);
+            canvas.drawRect(700, 100, 900, 200, p3);
+        }
+
+        if(state == SECOND) {
+            p1.setColor(Color.BLUE);
+            p2.setColor(Color.RED);
+            p3.setColor(Color.GREEN);
             canvas.drawARGB(255, 255, 255, 0);
-            canvas.drawRect(100, 100, 300, 200, p);
-        } else { //それ以外の場合は，Log にエラーを吐き出す
-            Log.d("error", "never come here");
+            canvas.drawRect(100, 100, 300, 200, p1);
+            canvas.drawRect(400, 100, 600, 200, p2);
+            canvas.drawRect(700, 100, 900, 200, p3);
+        }
+
+        if(state == THIRD) {
+            p1.setColor(Color.BLUE);
+            p2.setColor(Color.RED);
+            p3.setColor(Color.GREEN);
+            canvas.drawARGB(255, 255, 0, 255);
+            canvas.drawRect(100, 100, 300, 200, p1);
+            canvas.drawRect(400, 100, 600, 200, p2);
+            canvas.drawRect(700, 100, 900, 200, p3);
         }
     }
 
@@ -54,13 +74,13 @@ public class MainView extends View{
 
         //長方形の内部で
         if(x>100 && x < 300 && y>100 && y<200) {
-            if(state == FIRST){ //状態１だったら状態２へ
-                state = SECOND;
-            } else if (state == SECOND){ //状態 2 だったら状態 1 へ
-                state = FIRST;
-            } else { //それ以外だったらエラーを吐き出す
-                Log.d("error", "never come here");
-            }
+            state = FIRST;
+        }
+        if(x>400 && x < 600 && y>100 && y<200) {
+            state = SECOND;
+        }
+        if(x>700 && x < 900 && y>100 && y<200) {
+            state = THIRD;
         }
 
         invalidate(); //再描画
